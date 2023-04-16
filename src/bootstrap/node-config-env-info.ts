1e85ffa702ee285032f2072dfad3ffaf9f7824f9
vvmakarov <vvmakarov@corp.finam.ru>
2023-04-16 05:16:04 +0300

    2023-04-16--11

--- src/bootstrap/node-config-env-info.ts
+++ src/bootstrap/node-config-env-info.ts
@@ -5,7 +5,7 @@ export const nodeConfigEnvInfo = (arg?: {lineLen?: number, padding?: number}) =>
   const label = 'NODE_CONFIG_ENV:';
   const v = process.env.NODE_CONFIG_ENV;
   return v
-    ? `\n${bg.cyan}${green}${label}${' '.repeat(padding - label.length)}${
+    ? `${bg.cyan}${green}${label}${' '.repeat(padding - label.length)}${
       magenta}${v}${' '.repeat(lineLen - padding - v.length)}${rs}`
     : '';
 };
