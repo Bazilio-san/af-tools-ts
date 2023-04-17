29ad3ecf4229b821afdb02e422b4b42387bf23b4
vvmakarov <vvmakarov@corp.finam.ru>
2023-04-17 10:46:48 +0300

    2023-04-17--1

---
+++ __tests__/flattenObjectPrimitiveLeafs.js
@@ -0,0 +1,29 @@
+const obj = {
+  id: '123456',
+  ts: 1668860000553,
+  info_json: {
+    person: {
+      personName: 'Пупкин',
+    },
+    instrument: {
+      id: 9990,
+      name: 'instrument name',
+      type: 'opt',
+    },
+    signal: {
+      data: {
+        deviation: 57.97,
+        comparisonPrice: 17.377,
+      },
+      rule: {
+        threshold: 1,
+      },
+      liquidityInfo: {
+        deals: 2,
+      },
+    },
+  },
+  payload: { instrumentType: 'opt', comparisonPrice: 9999 },
+};
+const { flattenObjectPrimitiveLeafs } = require('../dist/cjs/src/object-utils.js');
+console.log(flattenObjectPrimitiveLeafs(obj, { keysType: 'mixed', noOverrideKeys: true }));
