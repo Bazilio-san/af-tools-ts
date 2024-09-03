b73a35186e7aa2c4778a62842109440e679605ea
vvmakarov <vvmakarov@corp.finam.ru>
2024-09-03 21:04:02 +0300

    1.0.87

--- src/bootstrap/databases-info.ts
+++ src/bootstrap/databases-info.ts
@@ -1,31 +1,33 @@
 import { magenta, rs, cyan, lBlue, underline, underlineOff } from 'af-color';
 import { TInfoLine } from '../interfaces';
 
-const getDbs = (dbs: any): TInfoLine<string>[] => {
+const getDbs = (dbs: any, filter?: string[]): TInfoLine<string>[] => {
   if (!dbs || !Object.keys(dbs).length) {
     return [];
   }
   const result: ([string, string] | string)[] = [];
-  Object.entries(dbs).forEach(([dbId, dbConfig]) => {
-    const { server: s, host: h, port: p, database: d, user: u } = dbConfig as any;
-    if ((!s && !h) || !d || !u) {
-      return;
-    }
-    const descr = `${cyan}${u}${rs}@${magenta}[${s || h}:${p}].[${lBlue}${underline}${d}${underlineOff}${magenta}]`;
-    result.push([`  ${underline}${dbId}${underlineOff}`, descr]);
-  });
+  Object.entries(dbs)
+    .filter(([dbId]) => !filter || filter.includes(dbId))
+    .forEach(([dbId, dbConfig]) => {
+      const { server: s, host: h, port: p, database: d, user: u } = dbConfig as any;
+      if ((!s && !h) || !d || !u) {
+        return;
+      }
+      const descr = `${cyan}${u}${rs}@${magenta}[${s || h}:${p}].[${lBlue}${underline}${d}${underlineOff}${magenta}]`;
+      result.push([`  ${underline}${dbId}${underlineOff}`, descr]);
+    });
   return result;
 };
 
-export const databasesInfo = (cfg: any): TInfoLine<string>[] => {
+export const databasesInfo = (cfg: any, filter?: string[]): TInfoLine<string>[] => {
   let result: TInfoLine<string>[] = [];
   const { database, db } = cfg;
 
   if (database) {
-    result = getDbs(database);
+    result = getDbs(database, filter);
   } else if (db) {
-    result = getDbs(db?.mssql?.dbs);
-    result.push(...getDbs(db?.postgres?.dbs));
+    result = getDbs(db?.mssql?.dbs, filter);
+    result.push(...getDbs(db?.postgres?.dbs, filter));
   }
   if (result.length) {
     result.unshift('Databases');
